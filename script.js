622e1d5676582de3fc655ed416646f8a858ed475
Depak-Debnath <depakdebnath000@gmail.com>
2026-08-23 02:13:06 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const mainTodoElem = document.querySelector(".todo-lists-elem");
+const inputValue = document.getElementById("inputValue");
+const addBtn = document.querySelector(".btn");
+
+inputValue.focus();
+
+const addCard = (todo, index) => {
+  let card = `<div class="todo-item">
+                <div class="main_todo_div">${todo}</div>
+                <button class="delete" data-index="${index}">Delete</button>
+            </div>`;
+
+  mainTodoElem.innerHTML += card;
+};
+
+const addTodoList = () => {
+  if (inputValue.value == "") {
+    alert("Please add some task!!");
+  } else {
+    let localTodoLists =
+      JSON.parse(localStorage.getItem("localTodoLists")) || [];
+
+    // if (!Array.isArray(localTodoLists)) {
+    //   localTodoLists = [];
+    // }
+
+    localTodoLists.push(inputValue.value);
+
+    localStorage.setItem("localTodoLists", JSON.stringify(localTodoLists));
+
+    // console.log(localTodoLists);
+
+    showTodoList();
+
+    inputValue.value = "";
+    inputValue.focus();
+  }
+};
+
+function addDeleteEvents() {
+  const deleteButtons = document.querySelectorAll(".delete");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      let localTodoLists =
+        JSON.parse(localStorage.getItem("localTodoLists")) || [];
+
+      const index = Number(button.dataset.index);
+
+      localTodoLists.splice(index, 1);
+
+      if (localTodoLists.length === 0) {
+        localStorage.removeItem("localTodoLists");
+      } else {
+        localStorage.setItem("localTodoLists", JSON.stringify(localTodoLists));
+      }
+
+      showTodoList();
+    });
+  });
+}
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addTodoList(e);
+});
+
+const showTodoList = () => {
+  let localTodoLists = JSON.parse(localStorage.getItem("localTodoLists")) || [];
+
+  mainTodoElem.innerHTML = "";
+
+  localTodoLists.forEach((todo, index) => {
+    addCard(todo, index);
+  });
+
+  addDeleteEvents();
+};
+
+showTodoList();
